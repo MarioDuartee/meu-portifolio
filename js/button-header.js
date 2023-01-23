25bef6f159d32ddef648cc00f1e44342985d5714
Mario Duarte <marioduartee@hotmail.com>
2023-01-23 18:08:05 -0300

    update  portfolio

--- js/button-header.js
+++ js/button-header.js
@@ -2,5 +2,5 @@ const buttonHeader = document.querySelectorAll('.btn-header')
 
 buttonHeader.forEach(btn => btn.addEventListener('click', function () {
     const typeBtn = this.value
-    typeBtn == 'curriculo' ? window.location.href = 'https://drive.google.com/file/d/1Gz4QugfSQS0dwU79sExv7kFZA1M3NnZZ/view' : window.location.href = 'https://wa.me/qr/3RCHVE5KAGELD1'
+    typeBtn == 'curriculo' ? window.location.href = 'https://drive.google.com/file/d/1ZaDkW2xoizxlZdtmc4hmPZG2emtPUp7-/view' : window.location.href = 'https://wa.me/qr/3RCHVE5KAGELD1'
 }))
